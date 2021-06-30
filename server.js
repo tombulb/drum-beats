377e471b010c2bf1262c5789de45acb7d67a0ee2
Anshuman Anupindi <anshuman.anupindi@gmail.com>
2021-06-30 14:55:01 +1000

    sessions

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const logger = require('./middlewares/logger.js');
 const port = 8080;
 const errorHandler = require('./middlewares/error_handler.js');
 // const beatsController = require('./controllers/beats_controller.js');
+var session = require('express-session');
+const { request } = require('express');
 
+// Use the session middleware
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
 
+// Access the session as req.session
+app.get('/', (req, res, next) => {
+  if (req.session.user_id) {
+    console.log('works');
+  } else {
+    req.session.user_id = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+  console.log(req);
+})
 
 app.listen(port, () => {
     console.log(`listening on port ${port} ...`);
